ef587c1890177e76228034706f340bc3d26f780c
hernan nequi <118672225+davidhernannequi@users.noreply.github.com>
2023-12-17 14:26:58 -0300

    Add files via upload

---
+++ Proyectos-Cursos/Proyectos-Udemy/WinFormsContacts/WinFormsContacts/WinFormsContacts/BussinesLogicLayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsContacts
+{
+    public class BussinesLogicLayer
+    {
+        private DataAccesLayer _dataAccesLayer;
+
+        public BussinesLogicLayer()
+        {
+            _dataAccesLayer = new DataAccesLayer();
+        }
+
+        public Contact SaveContact (Contact contact)
+        {
+            if (contact.Id == 0)
+                _dataAccesLayer.InsertContact(contact);
+            else
+                _dataAccesLayer.UpdateContact(contact);
+            
+            return contact;
+        }
+
+        public List<Contact> GetContacts(string searchText = null)
+        {
+            return _dataAccesLayer.GetContacts(searchText);
+        }
+
+        public void DeleteContact(int id)
+        {
+            _dataAccesLayer.DeleteContact(id);
+            }
+    }
+}
